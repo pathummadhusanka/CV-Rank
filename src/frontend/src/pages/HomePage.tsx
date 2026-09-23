616ec1c0a225603ce80ca1a807b14c22e8416557
Pathum Madhusanka <commits@pathummadhusanka.com>
2026-09-23 22:34:10 +0530

    chore(ui): install Shadcn

--- src/frontend/src/pages/HomePage.tsx
+++ src/frontend/src/pages/HomePage.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button"
+
+
 export default function HomePage() {
+	const [count, setCount] = useState(0);
 	return (
 		<main>
 			<h1>Home</h1>
 			<p className="bg-green-500">Welcome to the home page.</p>
+
+			<div className="mt-10 flex flex-row gap-4">
+				<span>Count is: {count}</span>
+				<Button onClick={() => setCount(count + 1)}>Click me {count}</Button>
+			</div>
 		</main>
 	);
 }
